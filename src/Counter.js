2bff512e0a442b3db4de0d4ce0fe3f9a60e1e69b
Viktoriia Zubarieva <viktoria.dubinina@gmail.com>
2023-01-06 14:54:20 -0800

    add useReducer hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer} from "react";
+
+
+const initialState = {
+  counter: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {
+        counter: state.counter + 1
+      };
+    default:
+      throw new Error(`There is no action matching ${action.type}.`);
+  }
+}
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  // Here we've replaced the useState hook originally used for counter state.
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [hidden, setHidden] = useState(false);
 
   useEffect(() => {
-    console.log("effect!");
-    document.title = counter;
-  }, [counter]);
+    // Now we need to access state.counter to get the counter value.
+    document.title = state.counter;
+  }, [state.counter]);
 
   return (
     <React.Fragment>
-      {hidden ? <h1>Count Hidden</h1> : <h1>{counter}</h1>}
-      <button onClick={() => setCounter(counter + 1)}>Count!</button>
+      {/* Same here: we need to access state.counter to get the counter value. */}
+      {hidden ? <h1>Count Hidden</h1> : <h1>{state.counter}</h1>}
+      {/* Now we use dispatch() to send an action to our reducer to update state. */}
+      <button onClick={() => dispatch({type: 'increment'})}>Count!</button>
       <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
     </React.Fragment>
   );
